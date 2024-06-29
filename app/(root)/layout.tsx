24a3b62336fdc8756c1a336538b3b621d8bdaf90
asimaries <logxtech59@gmail.com>
2024-06-29 23:07:36 +0530

    fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -21,13 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col min-h-screen`}>
+      <body className={inter.className}>
         <ClerkProvider>
           <ToasterProvider />
           <Navbar />
-          <main className="flex-grow">
-            {children}
-          </main>
+          {children}
         </ClerkProvider>
         <Footer />
       </body>
