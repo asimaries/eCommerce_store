800878140ca86676226a83fde00e31a42a17871d
asimaries <logxtech59@gmail.com>
2024-06-29 23:06:54 +0530

    footer placement

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -21,11 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
         <ClerkProvider>
           <ToasterProvider />
           <Navbar />
-          {children}
+          <main className="flex-grow">
+            {children}
+          </main>
         </ClerkProvider>
         <Footer />
       </body>
