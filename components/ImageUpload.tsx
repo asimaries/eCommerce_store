4476be187e7431b29b2256dc5927a4645de46a5d
asimaries <logxtech59@gmail.com>
2024-06-29 18:08:27 +0530

    image uploader preset

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -46,7 +46,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ itemId, value }) => {
         ))}
       </div>
 
-      <CldUploadWidget uploadPreset="fxsep0kz" onUpload={onUpload}>
+      <CldUploadWidget uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_PRESET} onUpload={onUpload}>
         {({ open }) => (
           <Button type="button" onClick={() => open()} className="text-white"
             variant={"default"}>
